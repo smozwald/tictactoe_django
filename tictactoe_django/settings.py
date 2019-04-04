2f2c8b8b8dab6e63954a63f95a3917ecae550626
Samuel Oswald <samuel.oswald1990@gmail.com>
2019-04-04 15:53:40 +0200

    Can't get CSS served

--- tictactoe_django/settings.py
+++ tictactoe_django/settings.py
@@ -123,3 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)          
+
+STATICFILES_FINDERS = (                                                         
+    'django.contrib.staticfiles.finders.FileSystemFinder',                      
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',                  
+)  
